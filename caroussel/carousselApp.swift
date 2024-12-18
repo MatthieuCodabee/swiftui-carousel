76709462862335b569868b0cc2045f8884e1b102
Matthieu <matthieupasserel@gmail.com>
2024-12-18 13:25:45 +0100

    Initial Commit

---
+++ caroussel/carousselApp.swift
@@ -0,0 +1,17 @@
+//
+//  carousselApp.swift
+//  caroussel
+//
+//  Created by matthieu passerel on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct carousselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
